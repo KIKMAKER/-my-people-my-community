496db8b1a0749716c2c51485bf9b51d48e1cffa3
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2022-11-08 14:10:01 +0200

    ARCH all routes created according to user stories

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :projects, except: :destroy
+  resources :pages, only: %i[show edit]
+  resources :posts, only: :create
+  resources :project_members, only: %i[create update]
+  resources :conversations, only:
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
