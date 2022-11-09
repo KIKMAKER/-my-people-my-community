819c601547980242a8fc20afdf9dbff1f180e4fa
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2022-11-09 12:36:18 +0200

    categories added to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,10 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :categories, through: :project_categories
   has_many :project_categories
+  has_many :categories, through: :project_categories
   has_many :posts
+  has_many :project_members
   has_many :users, through: :project_members
-  has_many :skills, through: :project_skills
   has_many :project_skills
+  has_many :skills, through: :project_skills
 end
