507d071331bacdcf49361b347b32890c1e386928
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2022-11-09 13:08:11 +0200

    update method and strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :bio, :location, :linkedin_url, :first_name, :last_name)
+  end
 end
