c1b269ca9559854ba65c86af94940a970f4de8f2
Carolyne Wamono <cwamono@gmail.com>
2022-11-10 18:32:35 +0200

    more of homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if params[:query].present?
+      @projects = Project.global_search(params[:query])
+    else
+      @projects = Project.all
+    end
   end
 end
