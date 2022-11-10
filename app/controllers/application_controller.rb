d3606db230c4645f99a23d3e87f20ef027bc6783
tracy sikenjana <unboxedcanvas@gmail.com>
2022-11-10 08:01:12 +0200

    Forms style completed, still needs to be tested in Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :bio, :location, :linkedin_url, :email, :password, :password_confirmation, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :bio, :location, :linkedin_url, :email, :password, :password_confirmation, :photo])
   end
 end
