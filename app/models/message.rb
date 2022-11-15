e2d9ae07bd27cd3aae0547704628cbaa650c9e60
tracy sikenjana <unboxedcanvas@gmail.com>
2022-11-15 13:27:36 +0200

    Finished chatroom style

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,9 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
+  def sender?(a_user)
+    user.id == a_user.id
+  end
+
   validates :content, presence: true
 end
