1b59be8db5c4e153fec786e7df31f4debd4895f3
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2022-11-17 13:49:46 +0200

    all rounder fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
- mclass User < ApplicationRecord
+ class User < ApplicationRecord
   has_many :conversations
   has_many :invitor_conversations, class_name: 'Conversation', foreign_key: 'invitor_id'
   has_many :invitee_conversations, class_name: 'Conversation', foreign_key: 'invitee_id'
@@ -57,7 +57,7 @@
 
   def all_rounder?
     skills_on_projects = projects.map { |project| project.skills }.flatten
-    skills_on_projects.count { |skill| skills.include?(skill) }
+    skills_on_projects.count { |skill| skills.include?(skill) } > 1
   end
 
   def expert?
