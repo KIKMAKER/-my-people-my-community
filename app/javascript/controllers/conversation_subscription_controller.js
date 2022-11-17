4d7803a40a8c721c7ff150613c3655e66eae2682
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2022-11-17 17:17:29 +0200

    project show page member

--- app/javascript/controllers/conversation_subscription_controller.js
+++ app/javascript/controllers/conversation_subscription_controller.js
@@ -14,38 +14,45 @@ export default class extends Controller {
           this.#insertMessageAndScrollDown(data);
         },
       }
-    )
-  };
+    );
+  }
   resetForm(event) {
     event.target.reset();
-  };
+  }
 
   #insertMessageAndScrollDown(data) {
     const currentUserIsSender = this.currentUserIdValue === data.sender_id;
-    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message);
-    
-    this.messagesTarget.insertAdjacentHTML("beforeend", data);
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
+    const messageElement = this.#buildMessageElement(
+      currentUserIsSender,
+      data.message
+    );
+
+    // this.messagesTarget.insertAdjacentHTML("beforeend", data);
+    // this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
 
     this.messagesTarget.insertAdjacentHTML("beforeend", messageElement);
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight);
-  };
+  }
 
   #buildMessageElement(currentUserIsSender, message) {
     return `
-      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+      <div class="message-row d-flex ${this.#justifyClass(
+        currentUserIsSender
+      )}">
         <div class="${this.#userStyleClass(currentUserIsSender)}">
           ${message}
         </div>
       </div>
-    `
-  };
+    `;
+  }
 
   #justifyClass(currentUserIsSender) {
-    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
-  };
+    return currentUserIsSender
+      ? "justify-content-end"
+      : "justify-content-start";
+  }
 
   #userStyleClass(currentUserIsSender) {
-    return currentUserIsSender ? "sender-style" : "receiver-style"
-  };
+    return currentUserIsSender ? "sender-style" : "receiver-style";
+  }
 }
--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,12 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import { Splide } from "@splidejs/splide";
 
 // Connects to data-controller="slider"
 export default class extends Controller {
   connect() {
     const splide = new Splide(this.element, {
-      perPage  : 2,
-      focus    : 'center',
+      perPage: 1,
+      focus: "center",
     });
     splide.mount();
   }
